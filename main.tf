81e3924296ed9a94c18ae1f7148ecb0bf60763b3
DivyadharshiniS <sdivya-cont@mahindra.com>
2022-09-07 18:20:46 +0530

    cloudflare module

--- main.tf
+++ main.tf
@@ -2,55 +2,71 @@ resource "azurerm_resource_group" "resource_group" {
   name     = var.rg-name
   location = var.location
 }
+# resource "azurerm_management_lock" "rg" {
+#   name       = var.rg-name
+#   scope      = azurerm_resource_group.resource_group.id
+#   lock_level = var.lock_level
+# }
 
-resource "azurerm_storage_account" "static-web-demo-storage" {
-  name                = var.storage-account-name
-  resource_group_name = azurerm_resource_group.resource_group.name
-
+resource "azurerm_storage_account" "static-web-storage" {
+  name                     = var.storage-account-name
+  resource_group_name      = azurerm_resource_group.resource_group.name
   location                 = azurerm_resource_group.resource_group.location
   account_tier             = var.account_tier
   account_replication_type = var.account_replication_type
   account_kind             = var.account_kind
 
   static_website {
-    index_document = var.index_document
+    index_document     = var.index_document
+    error_404_document = var.error_404_document
   }
 }
 
 #Add index.html to blob storage
-resource "azurerm_storage_blob" "static-web-demo-storage-blob" {
+resource "azurerm_storage_blob" "static-web-storage-blob" {
   name                   = var.index_document
-  storage_account_name   = azurerm_storage_account.static-web-demo-storage.name
-  storage_container_name = "$web"
-  type                   = "Block"
-  content_type           = "text/html"
+  storage_account_name   = azurerm_storage_account.static-web-storage.name
+  storage_container_name = var.storage_container_name
+  type                   = var.storage_type
+  content_type           = var.storage_content_type
   source_content         = var.source_content
 }
 
+#Add error.html to blob storage
+resource "azurerm_storage_blob" "static-web-storage-blob" {
+  name                   = var.error_404_document
+  storage_account_name   = azurerm_storage_account.static-web-storage.name
+  storage_container_name = var.storage_container_name
+  type                   = var.storage_type
+  content_type           = var.storage_content_type
+  source         = var.error_source
+
+}
+
 # CDN Profile
-resource "azurerm_cdn_profile" "static-web-demo-cdnprofile" {
+resource "azurerm_cdn_profile" "static-web-cdnprofile" {
   name                = var.cdnprofile-name
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
   sku                 = var.cdnprofile-sku
 
   tags = {
-    environment = "demo"
-    cost_center = "demo-cost-center"
+    environment = var.environment
+    cost_center = var.cost_center
   }
 }
 
 # CDN Endpoint
 resource "azurerm_cdn_endpoint" "static-web-endpoint" {
   name                = var.cdn-endpoint-name
-  profile_name        = azurerm_cdn_profile.static-web-demo-cdnprofile.name
+  profile_name        = azurerm_cdn_profile.static-web-cdnprofile.name
   location            = azurerm_resource_group.resource_group.location
   resource_group_name = azurerm_resource_group.resource_group.name
-  origin_host_header  = azurerm_storage_account.static-web-demo-storage.primary_web_host
+  origin_host_header  = azurerm_storage_account.static-web-storage.primary_web_host
 
   origin {
-    name      = "staticwebdemoportal"
-    host_name = azurerm_storage_account.static-web-demo-storage.primary_web_host
+    name      = var.rg-name
+    host_name = azurerm_storage_account.static-web-storage.primary_web_host
   }
 
   # Rules for Rules Engine
@@ -84,4 +100,32 @@ resource "azurerm_cdn_endpoint" "static-web-endpoint" {
       protocol      = "Https"
     }
   }
+}
+
+# resource "azurerm_dns_zone" "static-web" {
+#   name                = var.domain
+#   resource_group_name = azurerm_resource_group.resource_group.name
+# }
+
+# resource "azurerm_dns_cname_record" "static-web" {
+#   name                = var.cname
+#   zone_name           = azurerm_dns_zone.static-web.name
+#   resource_group_name = azurerm_dns_zone.static-web.resource_group_name
+#   ttl                 = var.ttl
+#   target_resource_id  = azurerm_cdn_endpoint.static-web-endpoint.id
+# }
+# resource "azurerm_cdn_endpoint_custom_domain" "static-web" {
+#   name            = var.cname
+#   cdn_endpoint_id = azurerm_cdn_endpoint.static-web-endpoint.id
+#   host_name       = "${azurerm_dns_cname_record.static-web.name}.${azurerm_dns_zone.static-web.name}"
+# }
+
+#Cloudflare
+module "modules" {
+  source             = "bootlabstech/modules/cloudflare"
+  version            = "1.0.1"
+  cloudflare_zone_id = var.cloudflare_zone_id
+  cloudflare_name    = var.cloudflare_name
+  cloudflare_value   = azurerm_cdn_endpoint.static-web-endpoint.id
+  cloudflare_type    = var.cloudflare_type
 }
--- variables.tf
+++ variables.tf
@@ -6,6 +6,21 @@ variable "rg-name" {
   description = "The name of the resource group"
 }
 
+# variable "lock_level" {
+#   description = "Specifies the Level to be used for this Lock. Possible values are CanNotDelete and ReadOnly. Changing this forces a new resource to be created"
+#   default     = "ReadOnly"
+# }
+
+variable "storage_container_name" {
+  description = "The name of the storage container in which this blob should be created"
+}
+variable "storage_content_type" {
+  description = "The content type of the storage blob. Cannot be defined if source_uri is defined. Defaults to application/octet-stream"
+}
+variable "storage_type" {
+  description = "The type of the storage blob to be created. Possible values are Append, Block or Page. Changing this forces a new resource to be created."
+}
+
 variable "storage-account-name" {
   description = "The name of the storage account"
 }
@@ -13,9 +28,15 @@ variable "storage-account-name" {
 variable "index_document" {
   description = "The index document of the static website"
 }
-
+variable "error_404_document" {
+  description = "The absolute path to a custom webpage that should be used when a request is made which does not correspond to an existing file"
+}
 variable "source_content" {
   description = "This is the source content for the static website"
+}
+
+variable "error_source" {
+  description = "An absolute path to a file on the local system. This field cannot be specified for Append blobs and cannot be specified if source_content or source_uri is specified"
 }
 
 variable "account_tier" {
@@ -43,4 +64,39 @@ variable "cdnprofile-sku" {
 
 variable "cdn-endpoint-name" {
   description = "Name of the CDN Endpoint Profile"
+}
+
+variable "environment" {
+  description = "Environment"
+}
+
+variable "cost_center" {
+  description = "Cost center"
+}
+
+# variable "ttl" {
+#   description = "The Time To Live (TTL) of the DNS record in seconds."
+# }
+
+# variable "cname" {
+#   description = "The name of the DNS CNAME Record"
+# }
+
+# variable "domain" {
+#   description = "The name of the DNS Zone. Must be a valid domain name"
+# }
+
+variable "cloudflare_zone_id" {
+  description = "Cloudflare Zone ID"
+  type        = string
+}
+
+variable "cloudflare_name" {
+  description = "Cloudflare record name"
+  type        = string
+}
+
+variable "cloudflare_type" {
+  description = "The type of the record"
+  type        = string
 }
