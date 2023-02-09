7b4854e348a316a0c99f014a816767abfeb491cf
lalitha2407 <lalitha@bootlabstech.com>
2023-02-09 09:36:25 +0530

    provider version changed

--- provider.tf
+++ provider.tf
@@ -1,10 +1,8 @@
 terraform {
-  required_version = ">=1.1.2"
-
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">=2.98.0"
+      version = "3.38.0"
     }
   }
 }
