d1212dd92d839554086e56b469f0695459893b11
lalitha2407 <lalitha@bootlabstech.com>
2023-05-05 12:47:35 +0530

    added additional config

--- main.tf
+++ main.tf
@@ -1,11 +1,10 @@
 data "azurerm_storage_account" "example" {
   name                = var.storage_account_name
   resource_group_name = var.resource_group_name
-  # location            = var.location
 }
 data "azurerm_storage_container" "example" {
   name                 = var.container_name
-  storage_account_name = var.storage_account_name
+  storage_account_name = data.azurerm_storage_account.example.name
 }
 resource "azurerm_storage_blob" "example" {
   count                  = var.no_of_blob
@@ -13,8 +12,8 @@ resource "azurerm_storage_blob" "example" {
   storage_account_name   = data.azurerm_storage_account.example.name
   storage_container_name = data.azurerm_storage_container.example.name
   type                   = var.type
-  size = var.size
-  # access_tier = var.access_tier
-  # source                 = "some-local-file.zip"
+  size                   = var.size
+  access_tier            = var.access_tier
+  parallelism            = var.parallelism
 
 }
--- variables.tf
+++ variables.tf
@@ -1,34 +1,50 @@
 variable "storage_account_name" {
   type        = string
   description = "value"
+
 }
 variable "resource_group_name" {
   type        = string
   description = "value"
+
 }
 variable "container_name" {
   type        = string
   description = "value"
+
+
 }
 variable "no_of_blob" {
-  type        = number
+  type        = string
   description = "value"
+
 }
 variable "name_of_blobs" {
   type        = list(string)
   description = "value"
+
 }
 variable "type" {
   type        = string
   description = "value"
-  default = "Page"
+
+
 }
 variable "size" {
-  type        = number
+  type        = string
   description = "value"
+  default = "0"
+
+
 }
-# variable "access_tier" {
-#   type        = string
-#   description = "value"
-#   default = "Hot"
-# }
+variable "access_tier" {
+  type        = string
+  description = "value"
+
+}
+variable "parallelism" {
+  type        = string
+  description = "value"
+  default = "8"
+
+}
