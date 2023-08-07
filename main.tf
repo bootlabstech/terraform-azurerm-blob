be41c3c7429956af43fd87dc4c571076751c75a1
lalitha2407 <lalitha@bootlabstech.com>
2023-08-07 12:02:17 +0530

    latest

--- main.tf
+++ main.tf
@@ -1,19 +1,24 @@
-data "azurerm_storage_account" "example" {
-  name                = var.storage_account_name
-  resource_group_name = var.resource_group_name
+resource "azurerm_storage_container" "example" {
+  name                  = var.sa_container_name
+  storage_account_name  = var.storage_account_name
+  container_access_type = var.container_access_type
+  lifecycle {
+    ignore_changes = [
+      tags,
+    ]
+  }
 }
-data "azurerm_storage_container" "example" {
-  name                 = var.container_name
-  storage_account_name = data.azurerm_storage_account.example.name
-}
+
 resource "azurerm_storage_blob" "example" {
-  count                  = var.no_of_blob
-  name                   = var.name_of_blobs[count.index]
-  storage_account_name   = data.azurerm_storage_account.example.name
-  storage_container_name = data.azurerm_storage_container.example.name
+  name                   = var.name
+  storage_account_name   = var.storage_account_name
+  storage_container_name = azurerm_storage_container.example.name
   type                   = var.type
   size                   = var.size
   access_tier            = var.access_tier
-  parallelism            = var.parallelism
-
+  lifecycle {
+    ignore_changes = [
+      tags,
+    ]
+  }
 }
--- variables.tf
+++ variables.tf
@@ -1,50 +1,39 @@
 variable "storage_account_name" {
   type        = string
-  description = "value"
+  description = "The name of the Storage Account where the Container should be created."
 
 }
-variable "resource_group_name" {
+variable "sa_container_name" {
   type        = string
-  description = "value"
+  description = "The name of the Container which should be created within the Storage Account."
+
 
 }
-variable "container_name" {
+variable "container_access_type" {
   type        = string
-  description = "value"
-
+  description = "The Access Level configured for this Container. Possible values are blob, container or private. Defaults to private."
 
 }
-variable "no_of_blob" {
+variable "name" {
   type        = string
-  description = "value"
-
-}
-variable "name_of_blobs" {
-  type        = list(string)
-  description = "value"
+  description = "Specifies the name of the storage account. Only lowercase Alphanumeric characters allowed. "
 
 }
 variable "type" {
   type        = string
-  description = "value"
+  description = "The type of the storage blob to be created. Possible values are Append, Block or Page"
 
 
 }
 variable "size" {
   type        = string
-  description = "value"
+  description = "Used only for page blobs to specify the size in bytes of the blob to be created. Must be a multiple of 512. Defaults to 0"
   default = "0"
 
 
 }
 variable "access_tier" {
   type        = string
-  description = "value"
-
-}
-variable "parallelism" {
-  type        = string
-  description = "value"
-  default = "8"
+  description = " The access tier of the storage blob. Possible values are Archive, Cool and Hot."
 
 }
